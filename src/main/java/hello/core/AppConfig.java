aa91833d6f5dad3adb004319ca71e872de53756d
jskim <js.story14@gmail.com>
2022-11-21 22:57:19 +0900

    - AppConfig.java 리팩토링 - ioc, DI, 컨테이너

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +13,19 @@ import hello.core.order.OrderServiceImple;
 public class AppConfig { //전체를 설정하고 구성하는 공연기획자같은 클래스
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입
+        return new MemberServiceImpl(memberRepository()); //생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImple(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImple(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
